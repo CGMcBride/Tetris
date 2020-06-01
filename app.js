5afe6d9766253cc4855ce7c192c12dfd7e81823f
CGMcBride <connermcbride@ymail.com>
2020-06-01 12:26:51 -0500

    added three more arrays for tetrominoes

--- app.js
+++ app.js
@@ -13,6 +13,45 @@ document.addEventListener("DOMContentLoaded", () => {
 		[1, width + 1, width * 2 + 1, width * 2],
 		[width, width * 2, width * 2 + 1, width * 2 + 2],
 	];
+
+	const zTetromino = [
+		[0, width, width + 1, width * 2 + 1],
+		[width + 1, width + 2, width * 2, width * 2 + 1],
+		[0, width, width + 1, width * 2 + 1],
+		[width + 1, width + 2, width * 2, width * 2 + 1],
+	];
+
+	const tTetromino = [
+		[1, width, width + 1, width + 2],
+		[1, width + 1, width + 2, width * 2 + 1],
+		[width, width + 1, width + 2, width * 2 + 1],
+		[1, width, width + 1, width * 2 + 1],
+	];
+
+	const oTetromino = [
+		[0, 1, width, width + 1],
+		[0, 1, width, width + 1],
+		[0, 1, width, width + 1],
+		[0, 1, width, width + 1],
+	];
+
+	const iTetromino = [
+		[1, width + 1, width * 2 + 1, width * 3 + 1],
+		[width, width + 1, width + 2, width + 3],
+		[1, width + 1, width * 2 + 1, width * 3 + 1],
+		[width, width + 1, width + 2, width + 3],
+	];
+
+	const theTetromino = [
+		lTetromino,
+		zTetromino,
+		tTetromino,
+		oTetromino,
+		iTetromino,
+	];
+
+	let currentPosition = 4;
+	let currentRotation = 0;
 });
 
 // create a function to use in array
